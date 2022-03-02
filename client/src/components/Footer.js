77ee2c51024f70c2036a2c0587ad6b984f574559
SEB_Full_36기_서외구 <89363048+sud665@users.noreply.github.com>
2022-03-02 23:04:23 +0900

    [Front]_Design_메인 Nav footer 레이아웃

---
+++ client/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Footer () {
+  /* TODO : Nav 만들기. */
+//리액트 라우터로 연결 해야 할것 
+  return (
+    <div>
+      <nav className="footer-container">
+        <div className="footer-item">김현규</div>
+        <div className="footer-item">이현걸</div>
+        <div className="footer-item">정다인</div>
+        <div className="footer-item">서외구</div>
+      </nav>
+    </div>
+  );
+}
+
+export default Footer;
