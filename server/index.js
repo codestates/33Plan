8f7886e200fe973090b567757385429f92c27c5e
ssankq <ssankq@gmail.com>
2022-03-02 21:08:06 +0900

    [Back]_Feat_HTTPS 서버 구현

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const dotenv = require("dotenv");
+dotenv.config();
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const usersRouter = require("./routes/users");
 const plannerRouter = require("./routes/planner");
-dotenv.config();
 
 const app = express();
-const port = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,6 +23,22 @@ app.use(cookieParser());
 app.use("/users", usersRouter);
 app.use("/planner", plannerRouter);
 
-app.listen(port, () => {
-  console.log(`Server listening on Port http://localhost:${port}`);
-});
+const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+
+let server;
+
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+  const credentials = { key: privateKey, cert: certificate };
+
+  server = https.createServer(credentials, app);
+  server.listen(HTTPS_PORT, () =>
+    console.log(`HTTPS SERVER LISTENING ON https://localhost:${HTTPS_PORT}`)
+  );
+} else {
+  server = app.listen(HTTPS_PORT, () =>
+    console.log(`현재 HTTP로 서버가 실행 중입니다 보안에 유의하세요`)
+  );
+}
+module.exports = server;
