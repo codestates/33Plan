70221f437481fece2580df76a030698f57b7c2af
hyeongirlife <leehyeongul@gmail.com>
2022-03-03 21:19:25 +0900

    [Back]_Feat_스키마를 참조한 테이블간 관계 구현

--- server/migrations/20220303114200-create-users.js
+++ server/migrations/20220303114200-create-users.js
@@ -1,21 +1,21 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('todays', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      id: {
+      nickname: {
         type: Sequelize.STRING
       },
-      user_id: {
-        type: Sequelize.INTEGER
+      email: {
+        type: Sequelize.STRING
       },
-      plan: {
-        type: Sequelize.TEXT
+      password: {
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('todays');
+    await queryInterface.dropTable('users');
   }
 };
