ebc850de262b899c2c601455148d69b48e1c8a6a
ssankq <ssankq@gmail.com>
2022-03-09 21:39:14 +0900

    [Deploy]_Env_환경변수 설정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-cd /home/ubuntu/33Plan/server
-authbind --deep pm2 start index.js
+cd /home/ubuntu/im-sprint-practice-deploy/server
+
+# export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
+export NODE_ENV=$(aws ssm get-parameters --region ap-northeast-2 --names NODE_ENV --query Parameters[0].Value | sed 's/"//g')
+
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export S3_URL=$(aws ssm get-parameters --region ap-northeast-2 --names S3_URL --query Parameters[0].Value | sed 's/"//g')
+export HTTPS_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names HTTPS_PORT --query Parameters[0].Value | sed 's/"//g')
+
+authbind --deep pm2 start app.js
