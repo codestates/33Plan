3d7ba3dcb139578259c2799cd7d023de72c8d98b
ssankq <ssankq@gmail.com>
2022-03-10 11:13:12 +0900

    [Deploy]_Env_DB HOST 수정

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
-    "host": "127.0.0.1",
+    "host": process.env.DATABASE_HOST,
     "dialect": "mysql"
   },
   "test": {
